99ad555540ff92ac4a7dd59d0ea6cc0b9d9f35ae
Cai miao <caimiao2017@outlook.com>
2018-11-12 06:37:16 +0000

    完成布局和路由

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title>@yield('title', 'Sample App') - Laravel 新手入门教程</title>
-</head>
-<body>
-  <h1>@yield('content')</h1>
-</body>
+  <head>
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    @include('layouts._header')
+    <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
+  </body>
 </html>
